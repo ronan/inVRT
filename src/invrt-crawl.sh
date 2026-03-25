44cd84d9174630dff1fe2f25828644551d4fb8a7
Ronan Dowling <ronan@dowlings.me>
2026-03-25 00:37:32 +0000

    Slight refinement of crawl script

--- src/invrt-crawl.sh
+++ src/invrt-crawl.sh
@@ -3,8 +3,8 @@
 
 echo "🕸️ Crawling '$INVRT_ENVIRONMENT' environment ($INVRT_URL) with profile: '$INVRT_PROFILE' to depth: $INVRT_MAX_CRAWL_DEPTH, max pages: $INVRT_MAX_PAGES"
 
-mkdir -p $INVRT_DATA_DIR/clones $INVRT_DATA_DIR/logs
-rm -rf $INVRT_DATA_DIR/clones/* $INVRT_DATA_DIR/logs/*
+mkdir -p $INVRT_DATA_DIR/clone $INVRT_DATA_DIR/logs
+rm -rf $INVRT_DATA_DIR/clone/* $INVRT_DATA_DIR/logs/*
 
 INVRT_DOMAIN=$(echo "$INVRT_URL" | sed -e 's|^[^/]*//||' -e 's|/.*$||')
 
@@ -24,30 +24,29 @@ if [ -f "$INVRT_DIRECTORY/exclude_urls.txt" ]; then
       EXCLUDE_URLS=$(cat $INVRT_DIRECTORY/exclude_urls.txt | grep -v '^\s*#'| tr '\n' ',' | sed 's/,$//')
       echo "Excluding URLs: $EXCLUDE_URLS"
 else
-      EXCLUDE_URLS='/files/sites,/user/logout';
+      EXCLUDE_URLS='/files,/sites,/user/logout';
       echo "No exclude_urls.txt file found at $INVRT_DIRECTORY/exclude_urls.txt. Default URLs will be excluded from crawling: $EXCLUDE_URLS"
 fi
 
 wget \
       --level=$INVRT_MAX_CRAWL_DEPTH \
+      --exclude-directories="$EXCLUDE_URLS" \
+      --domains=$INVRT_DOMAIN \
+      --directory-prefix=$INVRT_DATA_DIR/clone \
+      $cookie_option \
       --recursive \
       --max-redirect=3 \
       --user-agent=invrt/crawler \
-      $cookie_option \
-      --ignore-length \
-      --exclude-directories="$EXCLUDE_URLS" \
       --ignore-length \
       --no-verbose \
       --no-check-certificate \
       --reject=css,js,woff,jpg,png,gif,svg \
-      --directory-prefix=$INVRT_DATA_DIR/clones \
-      --no-directories \
       --no-host-directories \
-      --delete-after \
       --execute robots=off \
-      --domains=$INVRT_DOMAIN \
-      "$INVRT_URL" 2>&1 \
-      | tee $INVRT_DATA_DIR/logs/crawl.log \
+      "$INVRT_URL" 2> $INVRT_DATA_DIR/logs/crawl.log
+
+echo "Crawling finished. Parsing logs for unique urls..."
+cat $INVRT_DATA_DIR/logs/crawl.log \
       | grep "URL:$INVRT_URL" \
       | awk "//{gsub(\"URL:$INVRT_URL\", \"\"); print \$3}" \
       | sort \
