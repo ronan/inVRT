1e75d42b6c611a27701518fffd559b3d900fdccb
Ronan Dowling <ronan@dowlings.me>
2026-04-27 18:49:46 -0500

    Got the report builder working from inside the report builder directory
    cd /workspace/src/js/invrt-reporter && \
    INVRT_DIRECTORY=/workspaces/invrt/scratch/bluesky/.invrt/ npx astro
    build

--- src/js/invrt-reporter/astro.config.mjs
+++ src/js/invrt-reporter/astro.config.mjs
@@ -1,33 +1,14 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const srcdir = __dirname + '/src';
-
-console.log(__dirname);
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
+   build: {
     inlineStylesheets: 'always',
-  },
-  base: '/workspaces/invrt/src/js/invrt-reporter/src',
+  }, 
   vite: {
-    resolve: {
-      alias : {
-        "@/*": `./src/*`,
-      }
-    },
     plugins: [tailwindcss()],
-    build: {
-      emptyOutDir: false,
-      // Inline any small assets (e.g. icon SVGs) as data: URIs.
-      assetsInlineLimit: 1024 * 1024,
-    },
   },
+
 });
