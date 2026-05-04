42b7be9e80a412509c4c3868e18601dc390bceae
Ronan Dowling <ronan@dowlings.me>
2026-05-04 11:29:06 -0500

    Bumped to 0.4.0

--- src/cli/invrt.php
+++ src/cli/invrt.php
@@ -49,7 +49,7 @@ $app = new Application('
 ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░❚   ░██░██    ░██    ░██      ░██
 ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░❚    ░███     ░██     ░██     ░██
                     ❚
-                  ◀ █ ▶', '0.3.2');
+                  ◀ █ ▶', '0.4.0');
 
 $app->addCommand($container->get(InfoCommand::class));
 $app->addCommand($container->get(InitCommand::class));
